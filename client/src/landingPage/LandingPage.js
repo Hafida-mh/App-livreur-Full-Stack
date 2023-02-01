d56ac79aa7747e99ae0c5512987909c828e5cac0
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 11:17:14 +0100

    Update LandingPage.js

--- client/src/landingPage/LandingPage.js
+++ client/src/landingPage/LandingPage.js
@@ -2,18 +2,13 @@ import React from 'react'
 import '../landingPage/LandingPage.css'
 import { useState, useEffect } from 'react'
 export default function LandingPage() {
-
-
-
-
     const animate = (ct, y, c, w, h) => {
         const dy = 1;
         requestAnimationFrame(animate);
         ct.clearRect(0, 0, w, h)
         y -= dy
     }
-
-
+    
     const controlCanvas = () => {
         animate(ctx, y, canva, canva.width, canva.height);
         const canva = document.querySelector("#canva");
@@ -28,17 +23,12 @@ export default function LandingPage() {
         ctx.beginPath();
         ctx.fillRect(x, y, heig, wid);
         ctx.fill();
-
-     
-
-
     }
 
     useEffect(() => { controlCanvas() })
 
     return (
         <div className='landingPage'>
-
             <div>
                 <canvas id="canva"> </canvas>
             </div>
