7ef71f6710151b8f2ad10269eba32acb150c8b1f
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 11:57:31 +0100

    Update Footer.js

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -6,11 +6,8 @@ import LogoWhite from '../images/Logo_Plan de travail 1 (3).png'
 export default function Footer() {
   const {t} = useTranslation(["common"]);
   return (
- 
       <div className='contactContainer'>
-
-     <div> Copyright © 2022 - Developpé par H.M</div>
+         <div> Copyright © 2022 - Developpé par H.M</div>
       </div>
-    
   )
 }
