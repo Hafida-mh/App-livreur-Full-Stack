db7c185f8a621dfe9877b5ca51498d1d5b058655
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 13:43:37 +0100

    Update Confirmation.js

--- client/src/confirmerLivraison/Confirmation.js
+++ client/src/confirmerLivraison/Confirmation.js
@@ -4,22 +4,15 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 export default function Confirmation(props) {
-
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
-
 /*
     const getInfo = () => {
         axios.get('http://localhost:2000/appliv/verified/:id/:idClient/:todayDate/:timer/:current_minutes/:current_hours').then((res) => console.log(res));
     }
 */
-    useEffect(() => {
-     
-    }, []);
-
     return (
         <div className='confirmation'>
-
             <h1> Voici les informations du client </h1>
             <div> </div>
         </div>
