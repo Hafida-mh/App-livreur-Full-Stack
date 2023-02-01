1e8b3d6fa258f1c0028a263b9b1a5c77d5ae90ef
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 12:13:15 +0100

    Update Contact.js

--- client/src/contact/Contact.js
+++ client/src/contact/Contact.js
@@ -5,18 +5,15 @@ import { useState } from 'react'
 import Footer from '../footer/Footer'
 import { useTranslation } from 'react-i18next'
 export default function Contact() {
-
     const [nom, setNom] = useState();
     const [prenom, setPrenom] = useState();
     const [email, setEmail] = useState("");
     const { t } = useTranslation(["common"]);
     return (
         <div className='Contact'>
-
             <div className='navv'>
                 <NavBar />
             </div>
-
 
             <div className='ContactTile'>
                 <div> <h1> {t("esapcecontact")}   </h1>  </div>
@@ -25,53 +22,35 @@ export default function Contact() {
 
             <div className='formContainer'>
                 <div className='formContent'>
-
                     <form style={{ "marginTop": "20px" }} onSubmit={(e) => e.preventDefault()}>
-
                         <div className='formItem'>
                             <div className='nameForm'>
                                 <div className='formNomination'>   {t("nom")}  </div>
                                 <div> <input className='inputFiled' type="text" value={nom} onChange={(e) => setNom(e.target.value)} /></div>
                             </div>
-
                             <div className='nameForm'>
                                 <div className='formNomination'>   {t("prenom")}  </div>
                                 <div> <input className='inputFiled' type="text" value={prenom} onChange={(e) => setPrenom(e.target.value)} /></div>
                             </div>
                         </div>
-
-
                         <div className='formItemInfo'>
                             <div className='nameFormInfo'>
                                 <div className='formNomination'>   {t("email")}  </div>
                                 <div> <input className='inputFiledInfo' type="text" value={email} onChange={(e) => setEmail(e.target.value)} /></div>
                             </div>
                         </div>
-
                         <div className='formItemInfo'>
                             <div className='nameFormInfo'>
                                 <div className='formNomination'>   {t("message")} </div>
                                 <div> <input className='inputFiledInfo messageInput' type="text"  /*onChange={(e) => setTel(e.target.value)} value={tel}*/ /></div>
                             </div>
                         </div>
-
-
-
-
-
                         <div className='buttonContainer'> <div className='buttonConnexion' /*onClick={() => submit()}*/> <div className='button'>  {t("envoyer")}  </div></div> </div>
-
                     </form>
-
-
-
                 </div>
             </div>
 
-
             <div> <Footer /> </div>
-
-
         </div>
     )
 }
