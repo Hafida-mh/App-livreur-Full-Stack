1d43ae104279026c6a6fe77ed51669c1b9f539c5
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 11:03:21 +0100

    Update SwichConnexion.js

--- client/src/switchConnexion/SwichConnexion.js
+++ client/src/switchConnexion/SwichConnexion.js
@@ -10,12 +10,27 @@ import {
   } from "react-router-dom";
 export default function SwichConnexion() {
     return (
-        <div className='SwitchConnexion'>
-       <div> <h1> Identifiez-vous </h1> </div>  
-       <div> Vous êtes :</div>   
-       <div className='identity'> <h3> <li> <Link to="/loginLivreur"> Livreur  </Link> </li> </h3> </div>
-       <div className='identity'> <h3> <li> Transporteur </li> </h3> </div>
-       <div className='identity'> <h3> <li> <Link to="/connexionClient"> Professionnel/Particulier  </Link> </li> </h3> </div>
+       <div className='SwitchConnexion'>
+          <div> <h1> Identifiez-vous </h1> </div>  
+          <div> Vous êtes :</div>   
+          <div className='identity'> 
+             <h3> 
+                <li> 
+                   <Link to="/loginLivreur"> Livreur  </Link> 
+                </li> 
+             </h3> 
+          </div>
+       <div className='identity'>
+          <h3> 
+              <li> Transporteur </li> 
+          </h3> 
         </div>
+       <div className='identity'> 
+           <h3> 
+              <li> 
+                <Link to="/connexionClient"> Professionnel/Particulier  </Link> 
+             </li> </h3> 
+        </div>
+      </div>
     )
 }
